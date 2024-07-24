c213ca9d737e1d8d929f103f3e7f61b38c6bc2f8
setsunaki <wladimirburgos42@gmail.com>
2024-07-24 00:58:23 -0400

    Navbar y Home listo

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from "./Hero.module.css"
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hola, Soy wladimir</h1>
+            <p className={styles.description}>
+              Soy desarrollador full-stack con 2 años de experiancia(no formal) usando Python entre otros lenguajes.
+            </p>
+            <a href="mailto:wladimirburgos42@gmail.com" className={styles.contactBtn}>Contactame</a>
+        </div>
+        <img src={getImageUrl("hero/hero.jpg")} alt="hero imagen" className={styles.heroImg} />
+        <div className={styles.topBlur}></div>
+        <div className={styles.bottomBlur}></div>
+    </section>
+  )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+import styles from "./Navbar.module.css"
+import { getImageUrl } from '../../utils'
+
+
+export const Navbar = () => {
+    const [menuOpen, setMenuopen] = useState(false);
+
+  return(
+    <nav className={styles.navbar}>
+        <a className={styles.title} href='/'>
+            Portafolio
+        </a>
+        <div className={styles.menu}>
+            <img 
+                className={styles.menuBtn} 
+                src={
+                    menuOpen
+                        ? getImageUrl("nav/closeIcon.png")
+                        : getImageUrl("nav/menuIcon.png")
+                }
+                alt='menu-button'
+                onClick={() => setMenuopen(!menuOpen)}
+            />
+            <ul 
+                className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                onClick={() => setMenuOpen(false)}
+            >
+                <li>
+                    <a href="#about">Sobre mi</a>
+                </li>
+                <li>
+                    <a href="#about">Experiencia</a>
+                </li>
+                <li>
+                    <a href="#about">Proyectos</a>
+                </li>
+                <li>
+                    <a href="#about">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
